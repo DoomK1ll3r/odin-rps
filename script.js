d9999c7560dc10303fe0a3d32678987bb2531f57
Hazel Antonio Santos Quiroz <143685351+DoomK1ll3r@users.noreply.github.com>
2024-05-05 20:28:00 -0600

    Add playRound function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice (){
   const value = Math.floor(Math.random() * 3);
 
@@ -13,10 +16,57 @@ function getComputerChoice (){
 function getHumanChoice () {
   const regexOption = new RegExp('^(rock)|(paper)|(scissors)$', "i","m");
   let isValid = false;
+  let choice = prompt("Select (rock, paper, scissors)");
   do {
-    let choice = prompt("Select (rock, paper, scissors)");
     if (regexOption.test(choice)) {
       isValid = true;
     }
   } while (isValid == false);
+  return choice;
 }
+
+function playRound (humanChoice, computerChoice) {
+  let state = '';
+
+  if (humanChoice == "rock") {
+    if(computerChoice = "paper") {
+      computerScore += 1;
+      state = "lose";
+    }
+    else if (computerChoice == "scissors") {
+      humanScore += 1;
+      state = "win";
+    }
+    else {
+      state = "tie";
+    }
+  }
+  else if (humanChoice == "paper") {
+    if (computerChoice == "scissors") {
+      computerScore += 1;
+      state = "lose";
+    }
+    else if (computerChoice == "rock") {
+      humanScore += 1;
+      state = "win";
+    }
+    else {
+      state = "tie";
+    }
+  }
+  else {
+    if (computerChoice == "rock") {
+      computerScore += 1;
+      state = "lose";
+    }
+    else if (computerChoice == "paper") {
+      humanScore += 1;
+      state = "win";
+    }
+    else {
+      state = "tie";
+    }
+  }
+  console.log(`You ${state}\n You: ${humanChoice} Computer: ${computerChoice}\n Your Score: ${humanScore} Computer Score: ${computerScore}`);
+}
+
